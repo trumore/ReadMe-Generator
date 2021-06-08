fd588c371477369813d1be334bca8e90c2f606b7
Tom Rumore <rumoretom@gmail.com>
2021-06-08 16:55:43 -0400

    added inquirer prompts to index file, added initial structuring to generateMarkdown with examples listed in description section of formatting

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is the title of your project/application?",
+        name: "Title",
+    },
+    {
+        type: "input",
+        message: "Give a description of the purpose of your project/application.",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Are there any required installation instructions?",
+        name: "install",
+    },
+    {
+        type: "input",
+        message: "Are there any contributors on the project you would like to acknowledge?",
+        name: "contributors",
+    },
+    {
+        type: "input",
+        message: "Are there any tests for your application? If so, describe them here:",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub user name?",
+        name: "Github",
+    },
+    {
+        type: "input",
+        message: "What is a good email address to contact you at?",
+        name: "email",
+    },
+
+
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,6 +14,22 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}
+  ${data.install}
+
+  ## Installation
+
+  ## Usage
+
+  ## Contributing
+
+  ## Tests
+
+  ## Questions
+
+
+
 `;
 }
 
